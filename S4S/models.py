47594d3ae3a00194d529a18525787b14d7fa19f1
Ronc102 <ronco5@ac.sce.ac.il>
2024-03-02 17:30:26 +0200

    TestLoginRonc102

--- S4S/models.py
+++ S4S/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+class MyModel(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    email = models.EmailField()
--- S4S/views.py
+++ S4S/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'Home.html')
+
+def signup(request):
+    return render(request, 'SignUp.html')
+
+def forgotpassword(request):
+    return render(request, 'ForgotPassword.html')
