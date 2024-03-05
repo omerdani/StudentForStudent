9655380445dd47ed965952174379642262dec9a5
pedo <p3dobear69@gmail.com>
2024-03-05 15:12:32 +0200

    update 1.03 - (need to fix the login submit button)

--- S4S/views.py
+++ S4S/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render, redirect
 # Create your views here.
-
+from django.contrib.auth import authenticate, login as auth_login
 from django.shortcuts import render, redirect
 from .models import Candidate, Student, Graduate
-
 def signup(request):
     if request.method == 'POST':
         status = request.POST.get('status')
@@ -11,6 +10,13 @@ def signup(request):
         last_name = request.POST.get('lastName')
         email = request.POST.get('email')
         password = request.POST.get('password')
+
+        # Check if a user with the same email already exists
+        if Candidate.objects.filter(email=email).exists() or \
+                Student.objects.filter(email=email).exists() or \
+                Graduate.objects.filter(email=email).exists():
+            # If user with the same email already exists, return to signup page with an error message
+            return render(request, 'SignUp.html', {'error_message': 'User with this email already exists.'})
 
         if status == 'Candidate':
             Candidate.objects.create(first_name=first_name, last_name=last_name, email=email, password=password)
@@ -24,8 +30,7 @@ def signup(request):
         # Redirect to some page after successful signup
         return redirect('signup_success')  # Replace 'signup_success' with your URL name for signup success page
 
-    return render(request, 'SignUp.html')  # Replace 'home.html' with the actual path to your HTML template
-
+    return render(request, 'SignUp.html')  # Replace 'SignUp.html' with the actual path to your HTML template
 
 # Similarly, define views for signup and forgot password forms
 def display_data(request):
@@ -34,7 +39,24 @@ def display_data(request):
     graduates = Graduate.objects.all()
     return render(request, 'display_data.html', {'candidates': candidates, 'students': students, 'graduates': graduates})
 def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        # Authenticate the user
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            # If authentication is successful, log in the user
+            auth_login(request, user)
+            # Redirect to the home page
+            print(f"works")
+            return redirect('home')  # Assuming 'home' is the URL name for the home page
+        else:
+            # If authentication fails, return an error message or render the login page again
+            return render(request, 'Login.html', {'error_message': 'Invalid username or password'})
+
     return render(request, 'Login.html')
+
 def home(request):
     return render(request, 'Home.html')
 
