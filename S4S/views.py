07d22711b32ce5ba25e82bbe206ce453f3baba20
Yarin65 <159787291+Yarin65@users.noreply.github.com>
2024-03-07 13:31:50 +0200

    Update views.py

--- S4S/views.py
+++ S4S/views.py
@@ -3,34 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login as auth_login
 from django.shortcuts import render, redirect
 from .models import Candidate, Student, Graduate
-def signup(request):
-    if request.method == 'POST':
-        status = request.POST.get('status')
-        first_name = request.POST.get('name')
-        last_name = request.POST.get('lastName')
-        email = request.POST.get('email')
-        password = request.POST.get('password')
 
-        # Check if a user with the same email already exists
-        if Candidate.objects.filter(email=email).exists() or \
-                Student.objects.filter(email=email).exists() or \
-                Graduate.objects.filter(email=email).exists():
-            # If user with the same email already exists, return to signup page with an error message
-            return render(request, 'SignUp.html', {'error_message': 'User with this email already exists.'})
-
-        if status == 'Candidate':
-            Candidate.objects.create(first_name=first_name, last_name=last_name, email=email, password=password)
-        elif status == 'Student':
-            select_year = request.POST.get('year')
-            Student.objects.create(first_name=first_name, last_name=last_name, select_year=select_year, email=email, password=password)
-        elif status == 'Graduate':
-            work_place = request.POST.get('workplace')
-            Graduate.objects.create(first_name=first_name, last_name=last_name, work_place=work_place, email=email, password=password)
-
-        # Redirect to some page after successful signup
-        return redirect('signup_success')  # Replace 'signup_success' with your URL name for signup success page
-
-    return render(request, 'SignUp.html')  # Replace 'SignUp.html' with the actual path to your HTML template
 
 # Similarly, define views for signup and forgot password forms
 def display_data(request):
