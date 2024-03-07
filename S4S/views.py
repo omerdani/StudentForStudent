f9e1dff0987d5a40c7a9bc806ddf6b1bec897687
pedo <p3dobear69@gmail.com>
2024-03-07 12:19:43 +0200

    update 1.05

--- S4S/views.py
+++ S4S/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render, redirect
 # Create your views here.
-from django.contrib.auth import authenticate, login as auth_login
 from django.shortcuts import render, redirect
 from .models import Candidate, Student, Graduate
 def signup(request):
@@ -11,11 +9,11 @@ def signup(request):
         email = request.POST.get('email')
         password = request.POST.get('password')
 
-        # Check if a user with the same email already exists
+
         if Candidate.objects.filter(email=email).exists() or \
                 Student.objects.filter(email=email).exists() or \
                 Graduate.objects.filter(email=email).exists():
-            # If user with the same email already exists, return to signup page with an error message
+
             return render(request, 'SignUp.html', {'error_message': 'User with this email already exists.'})
 
         if status == 'Candidate':
@@ -27,12 +25,10 @@ def signup(request):
             work_place = request.POST.get('workplace')
             Graduate.objects.create(first_name=first_name, last_name=last_name, work_place=work_place, email=email, password=password)
 
-        # Redirect to some page after successful signup
-        return redirect('signup_success')  # Replace 'signup_success' with your URL name for signup success page
+        return redirect('signup_success')
 
-    return render(request, 'SignUp.html')  # Replace 'SignUp.html' with the actual path to your HTML template
+    return render(request, 'SignUp.html')
 
-# Similarly, define views for signup and forgot password forms
 def display_data(request):
     candidates = Candidate.objects.all()
     students = Student.objects.all()
@@ -40,23 +36,22 @@ def display_data(request):
     return render(request, 'display_data.html', {'candidates': candidates, 'students': students, 'graduates': graduates})
 def login(request):
     if request.method == 'POST':
-        username = request.POST.get('username')
+        email = request.POST.get('username')
         password = request.POST.get('password')
-        # Authenticate the user
-        user = authenticate(request, username=username, password=password)
 
-        if user is not None:
-            # If authentication is successful, log in the user
-            auth_login(request, user)
-            # Redirect to the home page
-            print(f"works")
-            return redirect('home')  # Assuming 'home' is the URL name for the home page
+        candidate = Candidate.objects.filter(email=email).first()
+        student = Student.objects.filter(email=email).first()
+        graduate = Graduate.objects.filter(email=email).first()
+        if candidate and candidate.password == password:
+            return redirect('')
+        elif student and student.password == password:
+            return redirect('')
+        elif graduate and graduate.password == password:
+            return redirect('')
         else:
-            # If authentication fails, return an error message or render the login page again
-            return render(request, 'Login.html', {'error_message': 'Invalid username or password'})
-
-    return render(request, 'Login.html')
-
+            return render(request, 'Login.html', {'error': 'Invalid email or password.'})
+    else:
+        return render(request, 'Login.html')
 def home(request):
     return render(request, 'Home.html')
 
