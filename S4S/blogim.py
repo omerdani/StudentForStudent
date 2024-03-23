318af4e28f4e6d0536bb1c474d10bbade27d663e
Ronc102 <ronco5@ac.sce.ac.il>
2024-03-23 13:12:32 +0200

    update 1.12

--- S4S/blogim.py
+++ S4S/blogim.py
@@ -134,3 +134,5 @@ def delete_comment(request, comment_id):
     comment.delete()
     return redirect('post_detail', post_id=post_id)
 
+def about_us(request):
+    return render(request, 'about_us.html')
