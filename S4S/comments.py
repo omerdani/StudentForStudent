c537603bc9426dc16fb602cb2854b0ab9c707af9
Ronc102 <ronco5@ac.sce.ac.il>
2024-03-26 19:33:01 +0200

    1.31

---
+++ S4S/comments.py
@@ -0,0 +1,12 @@
+
+from django.shortcuts import render, redirect
+from .models import Comment
+def edit_comment(request, comment_id):
+    comment = Comment.objects.get(id=comment_id)
+    if request.method == 'POST':
+        comment.text = request.POST.get('text')
+        comment.save()
+
+        return redirect('post_detail', post_id=comment.post.id)
+
+    return render(request, 'edit_comment.html', {'comment': comment})
